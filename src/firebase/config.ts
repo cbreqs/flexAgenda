4eddffbfa42ad20c413945cc9d155451b0be8873
Asteres Reqs <singularity@reqs.tech>
2026-03-09 23:54:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9670654404-bbdc8",
+  "appId": "1:760361367676:web:f7108bc486a6490c6ab1f9",
+  "apiKey": "AIzaSyAkHcHjXBcUXUSeJIqOZAznJpD3RpS_Na0",
+  "authDomain": "studio-9670654404-bbdc8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "760361367676"
+};
